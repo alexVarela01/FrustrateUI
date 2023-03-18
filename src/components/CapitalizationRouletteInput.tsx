fc20526e28aba9fc1ffe7e5d140bee052e539954
Alexandre <nawtz_sky@hotmail.com>
2023-03-18 15:17:55 +0000

    Fixed issue with input when Caps lock is ON

--- src/components/CapitalizationRouletteInput.tsx
+++ src/components/CapitalizationRouletteInput.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './css/CapitalizationRouletteInput.css'; // import the CSS file
 
 const ScreamingTextInput = ({ label, className, maxlength, value, onChange, chance = 0.2 }) => {
-  const [pressingShift, setPressingShift] = useState(false);
 
   const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
     const inputVal = e.currentTarget.value;
@@ -17,26 +16,10 @@ const ScreamingTextInput = ({ label, className, maxlength, value, onChange, chan
       if (Math.random() < chance) {
         onChange(value + newVal.toUpperCase());
       } else {
-        onChange(value + newVal.toLowerCase());
+        onChange(value + newVal);
       }
     } else {
       onChange(inputVal);
-    }
-  };
-
-  const handleKeyDown = (event) => {
-    if (event.code === "ShiftLeft") {
-      if(!pressingShift){
-        setPressingShift(true)
-      }
-    }
-  };
-
-  const handleKeyUp = (event) => {
-    if (event.code === "ShiftLeft") {
-      if(pressingShift){
-        setPressingShift(false)
-      }
     }
   };
 
@@ -44,7 +27,7 @@ const ScreamingTextInput = ({ label, className, maxlength, value, onChange, chan
     <div className={className}>
       <label className="random-label">{label}</label>
       <div className="random-input">
-        <input onKeyDown={handleKeyDown} onKeyUp={handleKeyUp} className="random-input__display" value={value} onChange={handleChange}/>
+        <input className="random-input__display" value={value} onChange={handleChange}/>
       </div>
     </div>
   );
